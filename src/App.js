321d9377185a6b0721a9f655e57fcac9c1f489a8
MD HIDAYAT <55959880+Hidayat78@users.noreply.github.com>
2023-11-08 15:03:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,17 @@ import { Footer } from "./components/Footer";
 function App() {
   return (
     <div className="App">
+    
       <NavBar />
+    
       <Banner />
+    
       <Skills />
+    
       <Projects />
+    
       <Contact />
+    
       <Footer />
     </div>
   );
